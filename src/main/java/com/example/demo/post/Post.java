db204610eca18c123374814560540789937bdebd
Choi Seung Ryeol <danielchoi1115@gmail.com>
2025-01-09 14:36:16 +0900

    DTO 수정

--- src/main/java/com/example/demo/post/Post.java
+++ src/main/java/com/example/demo/post/Post.java
@@ -3,17 +3,13 @@ package com.example.demo.post;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
-@Table("post")
 @AllArgsConstructor
 @Getter
 @Setter
 public class Post {
-    @Id
     private Long id;
 
     private String title;
--- src/main/java/com/example/demo/post/PostMeta.java
+++ src/main/java/com/example/demo/post/PostMeta.java
@@ -1,7 +1,14 @@
 package com.example.demo.post;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@Getter
+@Setter
 public class PostMeta {
     private int titleTextSize;
     private int contentTextSize;
